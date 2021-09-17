1753a575f45c41c0bd0b82bca524782a96b3e3b7
Amin Benarieb <amin.benarieb.work@gmail.com>
2021-09-17 23:17:32 +0300

    Fri Sep 17 23:15:10 MSK 2021

---
+++ lib/input_form_widget.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class InputFormWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      // theme: ThemeData(brightness: Brightness.dark),
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('Input Form'),
+      ),
+      child: SafeArea(child: InputForm()),
+      //actions: [IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved)]
+    );
+  }
+}
+
+class InputForm extends StatefulWidget {
+  @override
+  _InputFormState createState() => _InputFormState();
+}
+
+class _InputFormState extends State<InputForm> {
+  // Create a text controller and use it to retrieve the current value.
+  // of the TextField!
+  final myController = TextEditingController();
+  String? _errorText;
+
+  @override
+  void dispose() {
+    // Clean up the controller when disposing of the Widget.
+    myController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+          Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: CupertinoTextField(
+                controller: myController,
+                placeholder: "This is a hint",
+                onSubmitted: (String text) {
+                  setState(() {
+                    if (!isEmail(text)) {
+                      _errorText = 'Error: This is not an email';
+                    } else {
+                      _errorText = null;
+                    }
+                  });
+                },
+              )),
+          CupertinoButton(
+            child: Text("Show dialog"),
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    // Retrieve the text the user has typed in using our
+                    // TextEditingController
+                    content: Text(myController.text),
+                  );
+                },
+              );
+            },
+          ),
+          _getErrorTextWidget()
+        ])));
+    ;
+  }
+
+  Text _getErrorTextWidget() {
+    return Text(_errorText ?? "");
+  }
+
+  bool isEmail(String emailString) {
+    String emailRegexp =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+
+    RegExp regExp = RegExp(emailRegexp);
+
+    return regExp.hasMatch(emailString);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter/cupertino.dart';
 import 'random_words_table_widget.dart';
 import 'counter_button_widget.dart';
@@ -5,16 +7,26 @@ import 'animation_frame_widget.dart';
 import 'signature_canvas_widget.dart';
 import 'app_widgets_list_widget.dart';
 import 'json_data_list_widget.dart';
+import 'input_form_widget.dart';
 
 void main() {
-  runApp(CupertinoApp(
-    home: AppWidgetsListWidget(<String, String>{
-      "Signature canvas": "signature_canvas_widget",
-      "Animation frame": "animation_frame_widget",
-      "Counter button": "counter_button_widget",
-      "Random Words": "random_words_table_widget",
-      "Json data list ": "json_data_list_widget"
-    }),
+  runApp(MaterialApp(
+    home: Localizations(
+        locale: Locale('en', 'US'),
+        child: SafeArea(
+            child: AppWidgetsListWidget(<String, String>{
+          "Signature canvas": "signature_canvas_widget",
+          "Animation frame": "animation_frame_widget",
+          "Counter button": "counter_button_widget",
+          "Random Words": "random_words_table_widget",
+          "Json data list ": "json_data_list_widget",
+          "Input Form Widget": "input_list_widget"
+        })),
+        delegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          DefaultMaterialLocalizations.delegate
+        ]),
     routes: <String, WidgetBuilder>{
       '/random_words_table_widget': (BuildContext context) =>
           RandomWordsTableWidget(),
@@ -24,6 +36,7 @@ void main() {
       '/signature_canvas_widget': (BuildContext context) =>
           SignatureCanvasWidget(),
       '/json_data_list_widget': (BuildContext context) => JsonDataListWidget(),
+      '/input_list_widget': (BuildContext context) => InputFormWidget(),
     },
   ));
 }
